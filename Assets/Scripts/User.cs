2c2140bf54901f985083d950f64cd450f38387fe
Gregorio Hilman <gregorio.hilman@gmail.com>
2021-09-26 19:44:12 -0300

    Primer Commit

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,15 @@
+using System;
+
+[Serializable]
+public class User
+{
+    public string userName;
+    public int userScore;
+
+    public User()
+    {
+        userName = PlayerScores.playerName;
+        userScore = PlayerScores.playerScore;
+    }
+}
+
